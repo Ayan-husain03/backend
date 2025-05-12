19d168ad27f18fe5addd4ef2d62eb130e2f672d8
Ayan husain03 <hussainayan9897@gmail.com>
2025-05-12 20:25:32 +0530

    mongodb connect

---
+++ chai-backend/src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const connectDb = async () => {
+  try {
+    const connection = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n Mongodb connect !! DB Host : ${connection.connection.host}`
+    );
+  } catch (error) {
+    console.error("Mongodb connection failed : ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
---
+++ chai-backend/src/index.js
@@ -0,0 +1,10 @@
+import connectDb from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path : "./.env"
+})
+
+
+
+
+connectDb();
