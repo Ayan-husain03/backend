a86884d5c0951634cffaa90280bbf8fef7997503
Ayan husain03 <hussainayan9897@gmail.com>
2025-05-24 17:15:25 +0530

    add user register controller

--- chai-backend/src/controller/user.controller.js
+++ chai-backend/src/controller/user.controller.js
@@ -1,9 +1,55 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.service.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   //   get user details from frontend
+  // validate not empty
+  // check if user already exist
+  // check for images, check for avatar
+  // upload them to cloudinary
+  // create user object -create entry in db
+  // remove password and refresh token field from response
+  // check for user creation and return response
   const { fullname, email, username, password } = req.body;
-  console.log("Email : ", email);
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existUser)
+    throw new ApiError(409, "user with username or email is already exist");
+  // multer gives us by default req.files
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files.coverImage[0]?.path;
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar is required");
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) throw new ApiError(400, "Avatar is required");
+
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser)
+    throw new ApiError(500, "something went wrong while register a user");
+  // return the final response through Apiresponse method
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "user registered successfully"));
 });
 
 export { registerUser };
