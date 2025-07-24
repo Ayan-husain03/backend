94c2e78486a73e3c45b2e723118f6086e8a601fd
Ayan husain03 <hussainayan9897@gmail.com>
2025-07-24 16:39:58 +0530

    Add getUserChanelProfile controller to fetch user channel details and subscription status

--- chai-backend/src/controller/user.controller.js
+++ chai-backend/src/controller/user.controller.js
@@ -237,6 +237,67 @@ const updataUserAvatar = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "Avatar updated successfully", user));
 });
 
+const getUserChanelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) throw new ApiError(400, "user is not available");
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscriber",
+        },
+        channelSubscribedCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        channelSubscribedCount: 1,
+        isSubscribed: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) throw new ApiError(404, "channel doest not exists");
+  console.log(channel);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "user fetched successfully"));
+});
+
 export {
   registerUser,
   loginUser,
@@ -246,4 +307,5 @@ export {
   getCurrentUser,
   updataAccountDetails,
   updataUserAvatar,
+  getUserChanelProfile,
 };
